f3a8d676d8d0f1cf1373734b6175b73168cf7b45
MathieuBretaud <mathieu.bretaud@gmail.com>
2021-06-19 17:00:06 +0200

    fini

--- app.js
+++ app.js
@@ -10,8 +10,35 @@ let app = {
     ],
     // Méthode appelée au chargement de la page
     init: function() {
-     
-    }
+     let formulaire = document.querySelector('form');
+     let formulaires = formulaire.addEventListener('click', app.Handlerformulaire)
+    },
+
+    Handlerformulaire : function (evt){
+        evt.preventDefault();
+        let moninput = document.querySelector('input');
+        let moniputValeur = moninput.value;
+        let maDiv = document.querySelector('.speech');
+        const img = document.createElement('img');
+        let maphoto = "images/anthorvus.png";
+        
+
+        if(moniputValeur.length ===  8){
+            
+            maDiv.innerHTML = '<img src="images/maxopus.png" /> ';
+            
+        }else if(moniputValeur.charAt(0)=== 'l' || moniputValeur.substr(moniputValeur.length - 1) === 'x'){
+
+            maDiv.innerHTML = '<img src="images/lustrix.png" /> ';
+            
+        }else if(moniputValeur.length % 5 || moniputValeur.length % 3){
+
+            maDiv.innerHTML = '<img src="images/anthorvus.png" /> ';
+        } else if (moniputValeur.length >0) {
+
+            maDiv.innerHTML = '<img src="images/darioptera.png" /> ';
+        }
+    },
 }
 
 // Quand la page est entièrement chargée, on exécute la méthode init située dans l'object app.
